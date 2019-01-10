07f0ea71a7800ca3194e02e2a0d972c86540b320
sylwek <silva13@o2.pl>
2019-01-11 00:55:32 +0100

    first commit

--- Assets/Scripts/GameCamera.cs
+++ Assets/Scripts/GameCamera.cs
@@ -3,15 +3,108 @@ using System.Collections;
 
 public class GameCamera : MonoBehaviour
 {
+    private enum CameraState
+    {
+        IDLE,
+        SMOOTH_MOVE,
+        SAFEZONE_BOUNDS,
+    }
+
+    private CameraState currentCameraState = CameraState.IDLE;
+
+    // smooth move
+    private Vector2 smoothMoveDesiredPosition = Vector2.zero;
+    private float smoothMoveTime = 0.0f;
+    private float smoothMoveSpeed = 0.0f;
+
+    //
+    SafeZoneBounds safeZone;
+
+    struct SafeZoneBounds
+    {
+        public Vector2 areaSize;
+        public Rect boundaryRect;
+        public Bounds followBounds;
+        //float left, right, top, bottom;
+
+        public SafeZoneBounds(Bounds targetBounds, Vector2 size)
+        {
+            areaSize = size;
+            boundaryRect = new Rect(targetBounds.center, size);
+            followBounds = targetBounds;
+        }
+
+        public bool Update(Bounds bounds)
+        {
+            boundaryRect = new Rect(bounds.center, areaSize);
+            followBounds = bounds;
+            return false;
+        }
+    }
+
     // Use this for initialization
     void Start()
     {
 
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnDrawGizmos()
     {
+        Gizmos.color = new Color(0, 0, 1, .5f);
+        //Gizmos.DrawCube(safeZone.boundaryRect.center, safeZone.areaSize);
+        Gizmos.DrawCube(safeZone.followBounds.center, safeZone.areaSize);
+    }
 
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        bool movementDone = false;
+        switch(currentCameraState)
+        {
+            case CameraState.SMOOTH_MOVE:
+                movementDone = UpdateSmoothMove();
+                break;
+            case CameraState.SAFEZONE_BOUNDS:
+                //movementDone = safeZone.Update();
+                break;
+        }
+
+        if (movementDone)
+            currentCameraState = CameraState.IDLE;
+    }
+
+    public void SmoothMoveToPoint(Vector2 desiredPosition, float time, float speed)
+    {
+        smoothMoveDesiredPosition = desiredPosition;
+        smoothMoveTime = time;
+        smoothMoveSpeed = speed;
+        currentCameraState = CameraState.SMOOTH_MOVE;
+    }
+
+    private bool UpdateSmoothMove()
+    {
+        if(Vector2.Distance(transform.position, smoothMoveDesiredPosition) < 0.1f)
+        {
+            transform.position = smoothMoveDesiredPosition;
+            return true;
+        }
+
+        Vector2 velocity = Vector2.zero;
+        //Vector2 newPosition = Vector2.SmoothDamp(transform.position, smoothMoveDesiredPosition, ref velocity, smoothMoveTime, smoothMoveSpeed, Time.deltaTime);
+        Vector2 newPosition = Vector2.Lerp(transform.position, smoothMoveDesiredPosition, 0.05f);
+        transform.position = new Vector3(newPosition.x, newPosition.y, transform.position.z);
+
+        return false;
+    }
+
+    public void FollowWithSafeZone(Bounds targetBounds, Vector2 size)
+    {
+        safeZone = new SafeZoneBounds(targetBounds, size);
+        currentCameraState = CameraState.SAFEZONE_BOUNDS;
+    }
+
+    public void UpdateSafeZone(Bounds bounds)
+    {
+        safeZone.Update(bounds);
     }
 }
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,21 +4,50 @@ using System.Collections;
 public class GameManager : MonoBehaviour
 {
     //
-    public GameObject gameCamera = null;
+    public bool debugInfo = false;
+    public GameCamera gameCamera = null;
     public GameObject parralaxManager = null;
-    public GameObject myCatapult = null;
+    public Catapult myCatapult = null;
     public GameObject myCastle = null;
+    public Vector3 initialCameraPosition = Vector3.zero;
+    public Vector2 safeAreaSize = Vector2.zero;
+    public Rect safeAreaDeadzone = Rect.zero;
 
 
     // Use this for initialization
     void Start()
     {
+        gameCamera.GetComponent<GameCamera>().SmoothMoveToPoint(initialCameraPosition, 0.5f, 200.0f);
+    }
 
+    private void OnDrawGizmos()
+    {
+        if (!debugInfo)
+            return;
+
+        //var worldPosLeftTop = gameCamera.GetComponent<Camera>().ScreenToWorldPoint(new Vector2(safeAreaDeadzone.xMin * Screen.width, safeAreaDeadzone.yMin * Screen.height));
+        //var worldPosBottomRight = gameCamera.GetComponent<Camera>().ScreenToWorldPoint(new Vector2(safeAreaDeadzone.xMax * Screen.width, safeAreaDeadzone.yMax * Screen.height));
+
+        var position = gameCamera.GetComponent<Camera>().ScreenToViewportPoint(new Vector2(safeAreaDeadzone.center.x * Screen.width, safeAreaDeadzone.center.y * Screen.height));
+        var size = /*gameCamera.GetComponent<Camera>().ScreenToWorldPoint(*/new Vector2(safeAreaDeadzone.size.x * Screen.width, safeAreaDeadzone.size.y * Screen.height);
+
+        //Debug.Log("DSA " + size.ToString());
+        Debug.Log(Input.mousePosition.ToString());
+
+        Gizmos.color = new Color(0, 1, 0, .5f);
+        //Gizmos.DrawCube(safeAreaDeadzone.center, safeAreaDeadzone.size);
+        //Rect relative = new Rect()
+        Gizmos.DrawCube(position, size);
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetMouseButtonUp(0) == true)
+        {
+            gameCamera.FollowWithSafeZone(myCatapult.currentProjectile.GetComponent<Collider2D>().bounds, safeAreaSize);
+        }
 
+        gameCamera.UpdateSafeZone(myCatapult.currentProjectile.GetComponent<Collider2D>().bounds);
     }
 }
