8a12c283670e48a7906c086f3be0641270eeadbf
sylwek <silva13@o2.pl>
2019-01-13 13:05:57 +0100

    MouseRotate

---
+++ Assets/Scripts/MouseRotate.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseRotate : MonoBehaviour
+{
+    private float baseAngle = 0.0f;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnMouseDown()
+    {
+        Debug.Log("MouseDown");
+        Vector3 pos = Camera.main.WorldToScreenPoint(transform.position);
+        pos = Input.mousePosition - pos;
+        baseAngle = Mathf.Atan2(pos.y, pos.x) * Mathf.Rad2Deg;
+        baseAngle -= Mathf.Atan2(transform.right.y, transform.right.x) * Mathf.Rad2Deg;
+    }
+
+    void OnMouseDrag()
+    {
+        Vector3 pos = Camera.main.WorldToScreenPoint(transform.position);
+        pos = Input.mousePosition - pos;
+        float ang = Mathf.Atan2(pos.y, pos.x) * Mathf.Rad2Deg - baseAngle;
+        transform.rotation = Quaternion.AngleAxis(ang, Vector3.forward);
+    }
+}
